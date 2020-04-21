ef789351bdd6c620fffb2faae0e6899e84817789
theoc <a4319@mail.bg>
2020-04-21 22:29:52 +0300

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Login from "./components/Login/Login";
 import Status from "./components/status/Status";
+
 function App() {
+  const [token, setToken] = useState<null | string>(null);
+  const [errorMessage, setErrorMessage] = useState<null | string>(null);
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+  };
   return (
     <div className="App">
+      <Login setToken={setToken} setErrorMessage={setErrorMessage} />
+      <button onClick={logout}>logout</button>
       <Status />
     </div>
   );
