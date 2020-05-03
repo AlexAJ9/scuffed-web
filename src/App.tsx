46242470486c638455d0ebf98497ba9e9ff41ec7
theoc <a4319@mail.bg>
2020-05-04 01:31:18 +0300

    light/dark theme(fix colors)

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,12 @@ import { ProtectedRoute } from "./components/ProtectedRoute/ProtectedRoute";
 import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+
 import List from "@material-ui/core/List";
+import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
@@ -19,6 +24,7 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import HomeIcon from "@material-ui/icons/Home";
 import PersonIcon from "@material-ui/icons/Person";
+import WbIncandescentOutlinedIcon from "@material-ui/icons/WbIncandescentOutlined";
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -51,9 +57,37 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+const darkTheme = createMuiTheme({
+  palette: {
+    background: {
+      default: "#15202b",
+      paper: "#15202b",
+    },
+    text: {
+      primary: "#fff",
+    },
+    primary: {
+      main: "#1da1f2",
+    },
+    secondary: {
+      main: "#063255",
+    },
+  },
+});
+const lightTheme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: "#15202b",
+    },
+
+    primary: {
+      main: "#1da1f2",
+    },
+  },
+});
 function App() {
   const classes = useStyles();
-
+  const [theme, setTheme] = useState(lightTheme);
   const [token, setToken] = useState<null | string>(null);
   const [errorMessage, setErrorMessage] = useState<null | string>(null);
 
@@ -66,89 +100,109 @@ function App() {
     setToken(null);
     localStorage.clear();
   };
+  const handleChangeTheme = () => {
+    theme === lightTheme ? setTheme(darkTheme) : setTheme(lightTheme);
+  };
   return (
     <>
-      <Switch>
-        <ProtectedRoute token={token} exact path="/" Component={Status} />
-        <ProtectedRoute
-          token={token}
-          exact
-          path="/profile"
-          Component={Profile}
-        />
-        <Route
-          exact
-          path="/login"
-          component={() => (
-            <Login
-              setToken={setToken}
-              token={token}
-              setErrorMessage={setErrorMessage}
-            />
-          )}
-        />
-
-        <Route
-          exact
-          path="/register"
-          component={() => (
-            <Register token={token} setErrorMessage={setErrorMessage} />
-          )}
-        />
-        <Route component={Register} />
-
-        <button onClick={logout}>logout</button>
-      </Switch>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="left"
-      >
-        <div className={classes.toolbar} />
-        <Divider />
-        <List>
-          <Link
-            style={{
-              textDecoration: "none",
-              color: "#1da1f2",
-            }}
-            to="/"
-          >
-            <ListItem button>
-              <ListItemIcon>
-                <HomeIcon />
-              </ListItemIcon>
-              <ListItemText
-                primary={<Typography className={classes.text}>Home</Typography>}
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <ProtectedRoute token={token} exact path="/" Component={Status} />
+          <ProtectedRoute
+            token={token}
+            exact
+            path="/profile"
+            Component={Profile}
+          />
+          <Route
+            exact
+            path="/login"
+            component={() => (
+              <Login
+                setToken={setToken}
+                token={token}
+                setErrorMessage={setErrorMessage}
               />
-            </ListItem>
-          </Link>
-        </List>
-        <Divider />
-        <List>
-          <Link
-            style={{
-              textDecoration: "none",
-              color: "#1da1f2",
-            }}
-            to="/profile"
-          >
-            <ListItem button>
+            )}
+          />
+
+          <Route
+            exact
+            path="/register"
+            component={() => (
+              <Register token={token} setErrorMessage={setErrorMessage} />
+            )}
+          />
+          <Route component={Register} />
+
+          <button onClick={logout}>logout</button>
+        </Switch>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          anchor="left"
+        >
+          <div className={classes.toolbar} />
+          <Divider />
+          <List>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "#1da1f2",
+              }}
+              to="/"
+            >
+              <ListItem button>
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
+                <ListItemText
+                  primary={
+                    <Typography className={classes.text}>Home</Typography>
+                  }
+                />
+              </ListItem>
+            </Link>
+          </List>
+          <Divider />
+          <List>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "#1da1f2",
+              }}
+              to="/profile"
+            >
+              <ListItem button>
+                <ListItemIcon>
+                  <PersonIcon />
+                </ListItemIcon>
+                <ListItemText
+                  primary={
+                    <Typography className={classes.text}>Profile</Typography>
+                  }
+                />
+              </ListItem>
+            </Link>
+            <ListItem onClick={handleChangeTheme} button>
               <ListItemIcon>
-                <PersonIcon />
+                <IconButton>
+                  <WbIncandescentOutlinedIcon />
+                </IconButton>
               </ListItemIcon>
               <ListItemText
                 primary={
-                  <Typography className={classes.text}>Profile</Typography>
+                  <Typography className={classes.text}>Display</Typography>
                 }
               />
             </ListItem>
-          </Link>
-        </List>
-      </Drawer>
+          </List>
+        </Drawer>
+        <CssBaseline />
+      </ThemeProvider>
     </>
   );
 }
